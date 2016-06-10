c6936b46a3f7ad5b3d848c3c54d45668cc13973a
jillseidel <jseidel2017@users.noreply.github.com>
2016-06-10 12:30:54 -0500

    dashboard connected to each child

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,11 @@
 class DashboardController < ApplicationController
 
   def index
-  
+  @foods = Food.where(:child_id=>cookies[:child_id])
+  @sleeps = Sleep.where(:child_id=>cookies[:child_id])
+  @symptoms = Symptom.where(:child_id=>cookies[:child_id])
+  @words = Word.where(:child_id=>cookies[:child_id])
   end
+
 
 end
